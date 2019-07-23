03e9c6db51412799633e3276c19f1d0fc528e9ff
yzeng44 <47905122+yzeng44@users.noreply.github.com>
2019-07-23 15:03:43 -0400

    Create If-Else.py

---
+++ Python/Introduction/If-Else.py
@@ -0,0 +1,30 @@
+Task 
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+
+
+#!/bin/python3
+
+n = int(input())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
